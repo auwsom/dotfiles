1812a4e40bb69532d403196fdf9ad94a7138b85d
user <auwsom@users.noreply.github.com>
2023-01-24 17:52:08 -0800

    commit

--- autostart-scripts/mouse_move.sh
+++ autostart-scripts/mouse_move.sh
@@ -1,1 +1,12 @@
-while [ true ]; do xdotool mousemove 100 100; sleep 300; done
+#!/bin/bash
+
+while true; do
+HOUR="$(date +'%H')"; 
+if [ $HOUR -lt 20 -a $HOUR -gt 8 ] ; 
+then xdotool mousemove_relative 1 1; 
+fi; 
+sleep 3; 
+#echo mousemove;
+done 
+
+#while [ true ]; do xdotool mousemove 100 100; sleep 300; done
